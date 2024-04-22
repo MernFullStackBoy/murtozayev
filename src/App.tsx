9993fa29c6c7c814f6238791955bdef46c4e893e
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-04-22 20:56:34 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import LeftMenu from "./LeftMenu"
 import Modal from "./Modal"
 import UpperMenu from "./UpperMenu"
@@ -8,11 +8,14 @@ function App() {
 
   const [show, setShow] = useState<boolean>(false)
 
+  const [dark, setDark] = useState<boolean>(false)
+
+
   return (
     <div
-      className=" phone:max-w-[500px] phone:w-[100%] after:top-0 max-w-[1100px] w-[100%] mx-auto rounded-[10px] h-[100vh] bg-cyan-900 bg-back bg-cover after:backdrop-blur-[50px] after:w-[100%] after:max-w-[1100px] after:h-[100vh] after:absolute after:mx-auto after:rounded-[10px] after:backdrop-brightness-[0.2] "
+      className={` phone:max-w-[500px] transition-all phone:w-[100%] after:top-0 max-w-[1100px] w-[100%] mx-auto rounded-[10px] h-[100vh] ${dark ? "bg-cyan-900 bg-back" : "bg-white"} bg-cover after:backdrop-blur-[50px] after:backdrop-brightness-50 after:w-[100%] after:max-w-[1100px] after:h-[100vh] after:absolute after:mx-auto after:rounded-[10px] `}
     >
-      <UpperMenu handleClick={() => setShow(true)} />
+      <UpperMenu dark={dark} darkMode={() => setDark(prev => !prev)} handleClick={() => setShow(true)} />
       <div className=" flex gap-[30px] ">
         <LeftMenu />
         <RouteComponent />
--- src/UpperMenu.tsx
+++ src/UpperMenu.tsx
@@ -8,10 +8,11 @@ function UpperMenu(props: any) {
         >
             <img src={logo} className=" w-[50px] rounded-[50%] " alt="Logo" />
 
-            <button onClick={props.handleClick} className=" tablet:left-[70px] phone:hidden fas fa-heart relative right-[100px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
+            {/* <button onClick={props.handleClick} className=" tablet:left-[70px] phone:hidden fas fa-heart relative right-[100px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button> */}
 
-            <div className=" phone:flex gap-[30px] hidden pr-[25px] ">
-                <button onClick={props.handleClick} className=" tablet:left-[70px] phone:left-0 fas fa-heart relative left-[200px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
+            <div className=" flex gap-[30px] pr-[25px] ">
+                <button onClick={props.darkMode} className={`fa-solid ${props.dark ? "fa-sun" : "fa-moon"} w-[40px] h-[40px] rounded-[50%] text-[25px] ${props.dark ? "bg-white text-[black]" : "bg-black text-[white]"} `}></button>
+                <button onClick={props.handleClick} className=" phone:left-0 fas fa-heart relative w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
             </div>
         </nav>
     )
