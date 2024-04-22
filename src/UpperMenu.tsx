d62482dd38bc22976d45a556a020041b23d833a6
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-04-22 06:06:47 +0500

    Update

--- src/UpperMenu.tsx
+++ src/UpperMenu.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react"
 import logo from "./images/logo.png"
 
 function UpperMenu(props: any) {
-    const [hide, setHide] = useState<boolean>(true)
 
     return (
         <nav
@@ -10,22 +8,11 @@ function UpperMenu(props: any) {
         >
             <img src={logo} className=" w-[50px] rounded-[50%] " alt="Logo" />
 
-            <button onClick={props.handleClick} className=" tablet:left-[70px] phone:hidden fas fa-heart relative left-[200px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
+            <button onClick={props.handleClick} className=" tablet:left-[70px] phone:hidden fas fa-heart relative right-[100px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
 
             <div className=" phone:flex gap-[30px] hidden pr-[25px] ">
                 <button onClick={props.handleClick} className=" tablet:left-[70px] phone:left-0 fas fa-heart relative left-[200px] w-[40px] h-[40px] bg-[red] rounded-[10px] text-[1.3rem] "></button>
-                <button onClick={() => setHide(false)} className=" fa-solid fa-magnifying-glass text-[25px] "></button>
             </div>
-
-            <form className={`phone:${hide && "hidden"} phone:absolute phone:right-0 `}>
-                <input
-                    className=" max-w-[300px] pr-[20px] w-[300px] h-[40px] rounded-[10px] outline-none pl-[20px] "
-                    type="text"
-                    placeholder="Nimadir qidiring"
-                />
-                <button className=" fa-solid fa-magnifying-glass relative right-[40px] w-[40px] h-[40px] "></button>
-                <button type="button" onClick={() => setHide(true)} className=" fas fa-xmark hidden phone:block top-0 absolute right-0 text-[30px] "></button>
-            </form>
         </nav>
     )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,15 +5,10 @@ function Projects() {
 
     const [fav, setFav] = useState<object[]>([])
 
-
-    let items: any = localStorage.getItem("projects")
-
-    let project = JSON.parse(items)
-
-    const handleClick = (id: number) => {
-        project && project.map((item: any) => {
+    const handleClick = (id: number, prop: boolean) => {
+        projects && projects.filter((item: any) => {
             if (item.id === id) {
-                item.favourite = true
+                item.favourite = !prop
 
                 if (item.favourite) {
                     setFav([...fav, { ...item }])
@@ -28,14 +23,14 @@ function Projects() {
 
     return (
         <div className=" tablet:w-[100%] phone:w-[70vw] phone:h-[87vh] tablet:h-[87vh] max-w-[800px] w-[100vw] flex flex-wrap justify-between overflow-y-scroll mt-[20px] gap-[30px] h-[550px] ">
-            {projects.map((item: any) => {
+            {projects.map((item) => {
                 return (
-                    <div key={item.id} className=" w-[220px] tablet:w-[290px] tablet:h-[520px] rounded-[20px] bg-slate-900 h-[400px] ">
+                    <div key={item.id} className={`w-[220px] tablet:w-[290px] ${item.favourite ? "bg-yellow-500" : "bg-slate-900"} tablet:h-[520px] rounded-[20px] h-[400px]`}>
                         <img src={item.src} className=" w-[220px] tablet:h-[290px] tablet:w-[290px] rounded-[20px] h-[220px] " alt="Images" />
                         <h1 className=" text-center font-semibold text-[23px] tablet:text-[30px] mt-[20px] ">{item.title}</h1>
                         <h3 className=" text-center font-semibold tablet:text-[20px] mt-[10px] ">{item.desc}</h3>
                         <div className=" flex justify-center mt-[10px] tablet:mt-[20px] gap-[20px] ">
-                            <button onClick={() => handleClick(item.id)} className=" tablet:w-[50px] tablet:h-[50px] tablet:text-[25px] tablet:rounded-[20px] w-[30px] h-[30px] bg-[red] rounded-[5px] fas fa-heart "></button>
+                            <button onClick={() => handleClick(item.id, item.favourite)} className=" tablet:w-[50px] tablet:h-[50px] tablet:text-[25px] tablet:rounded-[20px] w-[30px] h-[30px] bg-[red] rounded-[5px] fas fa-heart "></button>
                             <button className=" w-[100px] tablet:w-[140px] tablet:h-[50px] tablet:text-[20px] h-[30px] shadow-[0_5px_2px_1px_darkblue] transition active:shadow-[none] bg-[blue] active:translate-y-[5px] font-semibold rounded-[10px] " onClick={() => window.location.assign(item.link)}>Kiramiz</button>
                         </div>
                     </div>
