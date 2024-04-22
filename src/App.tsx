1cb5ca7f09ee00c854d2b057f7d3c83a80f9aab6
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-04-22 20:16:45 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div
-      className=" after:top-0 max-w-[1100px] w-[100%] mx-auto rounded-[10px] h-[100vh] bg-cyan-900 bg-back bg-cover after:backdrop-blur-[50px] after:w-[100%] after:max-w-[1100px] after:h-[100vh] after:absolute after:mx-auto after:rounded-[10px] after:backdrop-brightness-[0.2] "
+      className=" phone:max-w-[500px] phone:w-[100%] after:top-0 max-w-[1100px] w-[100%] mx-auto rounded-[10px] h-[100vh] bg-cyan-900 bg-back bg-cover after:backdrop-blur-[50px] after:w-[100%] after:max-w-[1100px] after:h-[100vh] after:absolute after:mx-auto after:rounded-[10px] after:backdrop-brightness-[0.2] "
     >
       <UpperMenu handleClick={() => setShow(true)} />
       <div className=" flex gap-[30px] ">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ function Projects() {
     const [fav, setFav] = useState<object[]>([])
 
     const handleClick = (id: number, prop: boolean) => {
-        projects && projects.filter((item: any) => {
+        projects.filter((item: any) => {
             if (item.id === id) {
                 item.favourite = !prop
 
@@ -21,11 +21,18 @@ function Projects() {
         return localStorage.setItem("items", JSON.stringify(fav))
     }
 
+    let [filter, setFilter] = useState<string>("")
+
+    let dataSearch = projects.filter((item: any) => {
+        return Object.keys(item).some(key => item[key].toString().toLowerCase().includes(filter))
+    })
+
     return (
-        <div className=" tablet:w-[100%] phone:w-[70vw] phone:h-[87vh] tablet:h-[87vh] max-w-[800px] w-[100vw] flex flex-wrap justify-between overflow-y-scroll mt-[20px] gap-[30px] h-[550px] ">
-            {projects.map((item) => {
+        <div className=" tablet:w-[100%] phone:w-[70vw] phone:h-[87vh] tablet:h-[87vh] max-w-[800px] w-[100vw] flex flex-wrap overflow-y-scroll mt-[20px] gap-[50px] h-[550px] ">
+            <input type="text" className=" absolute font-semibold outline-none max-w-[300px] h-[40px] rounded-[10px] pl-[20px] pr-[20px] w-[100%] top-[30px] " value={filter} onChange={(e) => setFilter(e.target.value)} placeholder="Nimadir izlang" />
+            {dataSearch.map((item) => {
                 return (
-                    <div key={item.id} className={`w-[220px] tablet:w-[290px] ${item.favourite ? "bg-yellow-500" : "bg-slate-900"} tablet:h-[520px] rounded-[20px] h-[400px]`}>
+                    <div key={item.id} className={`w-[220px] mt-[60px] tablet:w-[290px] ${item.favourite ? "bg-yellow-500" : "bg-slate-900"} tablet:h-[520px] rounded-[20px] h-[400px]`}>
                         <img src={item.src} className=" w-[220px] tablet:h-[290px] tablet:w-[290px] rounded-[20px] h-[220px] " alt="Images" />
                         <h1 className=" text-center font-semibold text-[23px] tablet:text-[30px] mt-[20px] ">{item.title}</h1>
                         <h3 className=" text-center font-semibold tablet:text-[20px] mt-[10px] ">{item.desc}</h3>
